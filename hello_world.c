6e02c00e4b3b2e5de32e80640b6149d8b02c6da2
BRUTALHACKERPYTHON <114338908+BRUTALHACKERPYTHON@users.noreply.github.com>
2022-09-25 15:57:00 +0500

    C program hello world

---
+++ hello_world.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int a;
+    int b;
+    printf("Hello guys solve it\n");
+    printf("Enter the number\n");
+    scanf("%d",&a);
+    printf("Enter the second number\n");
+    scanf("%d",&b);
+    int c;
+    c=a+b;
+    printf("The some of two number is :%d",c);
+   
+}
